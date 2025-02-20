6a9e6a0b7e0c4bc1e6d2611b0b5f1d763e74ec46
deadManAlive <alfarrakhan@gmail.com>
2025-02-20 10:13:50 +0700

    abadeabade

--- d3/exercise/logic01/logic01.go
+++ d3/exercise/logic01/logic01.go
@@ -5,3 +5,13 @@ import "fmt"
 func Hello() {
 	fmt.Println("Hello!")
 }
+
+func Problem01(n int) []int {
+	arr := make([]int, n)
+
+	for i := range n {
+		arr = append(arr, 2*(i+1)+1)
+	}
+
+	return arr
+}
--- d3/exercise/main.go
+++ d3/exercise/main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/deadManAlive/amartha-stuff/d3/exercise/logic01"
 
 func main() {
-	logic01.Hello()
+	logic01.Problem01(10)
 }
